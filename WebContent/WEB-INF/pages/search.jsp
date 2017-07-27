2b4f0d6c20f184ff2ef5e159364de3dbb2cfd395
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2017-07-26 23:16:44 -0300

    Changes on the Search method and style on the search page it was added

--- WebContent/WEB-INF/pages/search.jsp
+++ WebContent/WEB-INF/pages/search.jsp
@@ -1,17 +1,32 @@
-<%@taglib uri="http://java.sun.com/jstl/core" prefix="c" %>
 <html lang="pt">
+<%@taglib uri="http://java.sun.com/jstl/core_rt" prefix="c" %>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="styles/styleTableCompanies.css"> 
     <title>Registered Companies</title>
 </head>
 <body>
     <h1 align = "center">Registered Companies</h1>
-       
-    <ul>
-        <c:forEach items="companies" var="company">
-		    <li>${company.getName()}</li>
-        </c:forEach>
-    </ul>
+     
+	<div align="center"> 
+		<table>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>NAME</th>
+					<th>EMAIL</th>
+				</tr>
+			</thead>	
+			<c:forEach var="company" items="${companies}">
+				<tr>
+					<td id="width_id_column">${company.id}</td>
+					<td id="width_name_column">${company.name}</td>
+					<td id="width_email_column">${company.email}</td>
+				</tr>
+			</c:forEach>
+		</table> 
+	<div align="center"> 
+    <p>
     <a href="/EnterpriseManager/registration.html">Add New Company</a>
 </body>
 </html>
--- src/web/SearchCompany.java
+++ src/web/SearchCompany.java
@@ -1,7 +1,6 @@
 package web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -21,41 +20,12 @@ public class SearchCompany extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		/*List<Company> companies = new CompanyDAO().list();
+		resp.setContentType("text/html");
+		List<Company> companies = new CompanyDAO().list();
 		req.setAttribute("companies", companies);
 				
 		RequestDispatcher disp = req.getRequestDispatcher("WEB-INF/pages/search.jsp");
-		disp.forward(req, resp);*/
-		
-		resp.setContentType("text/html");
-		PrintWriter out = resp.getWriter();
-		
-		String title = "Registered Companies";		
-			      
-	    out.println("<!DOCTYPE html>" +
-	         "<html>\n" +
-	         "<head><title>" + title + "</title></head>\n" +
-	         "<body bgcolor = \"#f0f0f0\">\n" +
-	         "<h1 align = \"center\">" + title + "</h1>\n");
-	    
-	    out.println("<ul>");
-	    
-		List<Company> listCompany = new CompanyDAO().list();
-		
-		for (Company comp : listCompany) {
-			// Add a new list item.
-			out.println("<li>");
-			// Display the values.
-			out.println("ID: "+comp.getId());
-			out.println(" NAME: "+comp.getName());
-			out.println(" EMAIL: "+comp.getEmail());	
-			
-			out.println("</li>");
-		}
-		out.println("</ul>");
-		out.println("<p>");
-		out.println("<a href=\"/EnterpriseManager/registration.html\">Add New Company</a>");
-		out.println("</body></html>");
+		disp.forward(req, resp);
 	}
 	
 }
